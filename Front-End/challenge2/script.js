04e7d7d2d71683a4ec907871ed82a6c9330a0747
DianaSanchezordonez <dianaordonez1998@gmail.com>
2021-04-12 23:34:45 -0500

    reduced lines of code

--- Front-End/challenge2/script.js
+++ Front-End/challenge2/script.js
@@ -9,23 +9,6 @@ TO-DO:
 const wrapper = document.querySelector('.wrapper');
 const result = document.querySelector('#display');
 
-// const zero = document.getElementById('zero');
-// const one = document.getElementById('one');
-// const two = document.getElementById('two');
-// const three = document.getElementById('three');
-// const four = document.getElementById('four');
-// const five = document.getElementById('five');
-// const six = document.getElementById('six');
-// const seven = document.getElementById('seven');
-// const eight = document.getElementById('eight');
-// const nine = document.getElementById('nine');
-
-// const division = document.getElementById('division');
-// const multiplication = document.getElementById('multiplication');
-// const subtrack = document.getElementById('subtrack');
-// const add = document.getElementById('add');
-// const equals = document.getElementById('equals');
-
 const calculatorButtons = document.querySelectorAll('button');
 
 let numberA, numberB, operator;
@@ -47,76 +30,7 @@ calculatorButtons.forEach( button => {
             calculator();
         })
     }
-})
-
-// zero.addEventListener('click', (e) => {
-//     result.innerText = result.innerText + '0';
-// });
-
-// one.addEventListener('click', (e) => {
-//     result.innerText = result.innerText + '1';
-// });
-
-// two.addEventListener('click', (e) => {
-//     result.innerText = result.innerText + '2';
-// });
-
-// three.addEventListener('click', (e) => {
-//     result.innerText = result.innerText + '3';
-// });
-
-// four.addEventListener('click', (e) => {
-//     result.innerText = result.innerText + '4';
-// });
-
-// five.addEventListener('click', (e) => {
-//     result.innerText = result.innerText + '5';
-// });
-
-// six.addEventListener('click', (e) => {
-//     result.innerText = result.innerText + '6';
-// });
-
-// seven.addEventListener('click', (e) => {
-//     result.innerText = result.innerText + '7';
-// });
-
-// eight.addEventListener('click', (e) => {
-//     result.innerText = result.innerText + '8';
-// });
-
-// nine.addEventListener('click', (e) => {
-//     result.innerText = result.innerText + '9';
-// });
-
-// add.addEventListener('click', (e) => {
-//     numberA = result.innerText;
-//     operator = '+';
-//     clean();
-// });
-
-// subtrack.addEventListener('click', (e) => {
-//     numberA = result.innerText;
-//     operator = '-';
-//     clean();
-// });
-
-// multiplication.addEventListener('click', (e) => {
-//     numberA = result.innerText;
-//     operator = 'X';
-//     clean();
-// });
-
-// division.addEventListener('click', (e) => {
-//     numberA = result.innerText;
-//     operator = '/';
-//     clean();
-// });
-
-// equals.addEventListener('click', (e) => {
-//     numberB = result.innerText;
-//     calculator();
-// })
+});
 
 function clean(){
     result.innerText = '';
